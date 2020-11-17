dcc3f5de144b4f92edc7e303a5848772158dcce5
Stephen Kline <Stephenkline09@gmail.com>
2020-11-17 14:03:54 -0600

    Added MagicButton and owenWilsonButton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatDividerModule } from '@angular/material/divider';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition} from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   colorBoolean: boolean = false
   randomColor: string;
   textColor: string;
   backgroundColor: string
+  randomPic: string
+  randomAudioClip: string
+  isVisible: boolean = true
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  vanishButton() {
+    this.isVisible = !this.isVisible //Not sure if this is correct
+  }
+
   changePicture() {
-    var img = new Image();
-    img.src='./assets/images/owen_wilson/wow.jpg';
-    document.getElementById('wow');
+    const randomPicArray = [
+      './assets/images/owen_wilson/wow.jpg',
+      './assets/images/owen_wilson/wow_2.jpg',
+      './assets/images/owen_wilson/wow_3.jpg',
+      './assets/images/owen_wilson/wow_4.jpg',
+      './assets/images/owen_wilson/wow_5.jpg',
+      './assets/images/owen_wilson/wow_6.jpg'
+    ];
+    this.randomPic = randomPicArray[Math.floor(Math.random() * randomPicArray.length)]
   }
 
   switchBackground() {
@@ -55,5 +71,25 @@ export class HomeComponent implements OnInit {
   enlarge() {
     //size-button-container ++ height and width &&move position right
   }
+
+  owenWilsonAudio() {
+    const randomOwenWilsonAudioArray = [
+    './assets/audioClips/owen_wilson_audio/hadThisTheory.mp3',
+    './assets/audioClips/owen_wilson_audio/iCareDesperately.mp3',
+    './assets/audioClips/owen_wilson_audio/imInHillBillyHell.mp3',
+    './assets/audioClips/owen_wilson_audio/threePointFiveYears.mp3'
+    ]
+  this.randomAudioClip = randomOwenWilsonAudioArray[Math.floor(Math.random() * randomOwenWilsonAudioArray.length)]
+  this.playAudio()
+  }
+
+  playAudio(){
+    let audio = new Audio();
+    audio.src = this.randomAudioClip;
+    audio.load();`1 `
+    audio.play();
+  }
+
+
 
 }
